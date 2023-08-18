1fb5f89d148b9edef4f32e7a048d2505fe923dde
dadadon <davianberoni@yahoo.com>
2023-08-18 14:04:11 -0500

    Add localhost to safelist

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,8 +1,8 @@
 class Rack::Attack
-    # # Allow all requests from localhost
-    # safelist('allow-localhost') do |req|
-    #   '127.0.0.1' == req.ip || '::1' == req.ip
-    # end
+    # Allow all requests from localhost
+    safelist('allow-localhost') do |req|
+      '127.0.0.1' == req.ip || '::1' == req.ip
+    end
   
     # Limit requests to the calculate_all_taxes endpoint to 100 requests per hour per IP
     throttle('calculate_all_taxes', limit: 50, period: 1.hour) do |req|
